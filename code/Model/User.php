ef67009c92325155a90e8aef9a3cdc2cba11ab6e
Dustin Doiron <dustin@dustindoiron.com>
2015-03-15 20:16:17 -0400

    Whoops, missed docblocks for User model

--- code/Model/User.php
+++ code/Model/User.php
@@ -1,11 +1,11 @@
 <?php
 /**
+ * Model\User is our the model that represents a User on NeatStuff
  *
- *
- *
- *
- *
- *
+ * @package NeatstuffReference
+ * @subpackage Model
+ * @author Dustin Doiron <dustin@weebly.com>
+ * @since 
  */
 namespace Model;
 
@@ -27,11 +27,24 @@ class User extends Base
 
   protected $pk = 'user_id';
 
+  /**
+   * Returns the current primary key for this model
+   *
+   * @return string
+   */
   public function getPrimaryKey( )
   {
     return $this->pk;
   }
 
+  /**
+   * Constructor, creates a model based on the given primary key value
+   *
+   * @param mixed $pkValue
+   *
+   * @return object
+   * @throws \Exception
+   */
   public function __construct( $pkValue = NULL )
   {
     if ( is_numeric( $pkValue ) === true ) {
@@ -45,6 +58,14 @@ class User extends Base
     parent::__construct( $pkValue );
   }
 
+  /**
+   * Generates a password hash with the given plaintext password
+   *
+   * @param string $password
+   *
+   * @return string
+   * @throws \Exception
+   */
   public function generatePasswordHash( $password )
   {
     if ( isset( $password{4} ) === false ) {
@@ -60,6 +81,13 @@ class User extends Base
     );
   }
 
+  /**
+   * Validates that the given plaintext password hashes to the current model's password hash
+   *
+   * @param string $password
+   *
+   * @return bool
+   */
   public function validatePassword( $password )
   {
     if ( isset( $password ) === false ) {
